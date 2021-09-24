5643f0c2af2daa17ee7945ef33569d0301b0d692
Skylar Harwell <swharwell@gmail.com>
2021-09-24 08:48:48 -0400

    Updated PUT routes for completion

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -47,40 +47,16 @@ router.post('/', async (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
-  Tag.update(req.body, {
-    where: {
-      id: req.params.id,
+  Tag.update({
+      tag_name: req.body.tag_name,
     },
+    {
+      where: {id: req.params.id},
   })
-    .then((tag) => {
-      return Tag.findAll({ where: { id: req.params.id } });
+    .then((tagData) => { 
+      res.status(200).json(tagData);
     })
-    .then((productTags) => {
-      // get list of current tag_ids
-      const productTagIds = productTags.map(({ tag_id }) => tag_id);
-      // create filtered list of new tag_ids
-      const newProductTags = req.body.tagIds
-        .filter((tag_id) => !productTagIds.includes(tag_id))
-        .map((tag_id) => {
-          return {
-            product_id: req.params.id,
-            tag_id,
-          };
-        });
-      // figure out which ones to remove
-      const productTagsToRemove = productTags
-        .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
-        .map(({ id }) => id);
-
-      // run both actions
-      return Promise.all([
-        ProductTag.destroy({ where: { id: productTagsToRemove } }),
-        ProductTag.bulkCreate(newProductTags),
-      ]);
-    })
-    .then((updatedProductTags) => res.json(updatedProductTags))
     .catch((err) => {
-      // console.log(err);
       res.status(400).json(err);
     });
 });
@@ -99,7 +75,7 @@ router.delete('/:id', async (req, res) => {
       return;
     }
 
-    res.status(200).json(tagtData);
+    res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
   }
